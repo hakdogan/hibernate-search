c66225223fbf46d14ee1a85e9861e4123c414bc6
hakdogan <1975_Hakdogan_1453>
2020-11-29 23:23:16 +0300

    import.sql updated

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,9 @@
-INSERT INTO event(id, name) VALUES ('1', 'Introduction Quarkus');
-INSERT INTO event(id, name) VALUES ('2', 'Debezium');
-INSERT INTO event(id, name) VALUES ('3', 'Terelelli');
+INSERT INTO event(id, name) VALUES (1, 'Event Storming vs Event Modeling');
+INSERT INTO event(id, name) VALUES (2, 'CI & CD with Azure Devops');
+INSERT INTO event(id, name) VALUES (3, 'Introduction to Quarkus');
+INSERT INTO event(id, name) VALUES (4, 'Change Data Capture with Debezium and Apache Kafka');
+
+INSERT INTO host(id, name, title, event_id) VALUES (nextval('hibernate_sequence'), 'Rafal Maciag', 'CQRS/ES philosophy advocate', 1);
+INSERT INTO host(id, name, title, event_id) VALUES (nextval('hibernate_sequence'), 'Huseyin Akdogan', 'Expert Software Consultant', 2);
+INSERT INTO host(id, name, title, event_id) VALUES (nextval('hibernate_sequence'), 'Huseyin Akdogan', 'Expert Software Consultant', 3);
+INSERT INTO host(id, name, title, event_id) VALUES (nextval('hibernate_sequence'), 'Aykut Bulgu', 'System Craftsman', 4);
