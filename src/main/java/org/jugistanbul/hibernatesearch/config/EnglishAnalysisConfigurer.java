aafa9232b5eddd35cb80f261a993cb92b1666f81
Hüseyin Akdoğan <hakdogan@kodcu.com>
2020-11-30 09:59:34 +0300

    EnglishAnalysisConfigurer added

---
+++ src/main/java/org/jugistanbul/hibernatesearch/config/EnglishAnalysisConfigurer.java
@@ -0,0 +1,28 @@
+package org.jugistanbul.hibernatesearch.config;
+
+import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurationContext;
+import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurer;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hakdogan (hakdogan@kodcu.com)
+ * Created on 30.11.2020
+ **/
+@Component
+public class EnglishAnalysisConfigurer implements ElasticsearchAnalysisConfigurer
+{
+    @Override
+    public void configure(ElasticsearchAnalysisConfigurationContext context) {
+        context.analyzer( "english" ).custom()
+                .tokenizer( "standard" )
+                .tokenFilters( "lowercase", "snowball_english", "asciifolding" );
+
+        context.tokenFilter( "snowball_english" )
+                .type( "snowball" )
+                .param( "language", "English" );
+
+        context.analyzer( "name" ).custom()
+                .tokenizer( "standard" )
+                .tokenFilters( "lowercase", "asciifolding" );
+    }
+}
