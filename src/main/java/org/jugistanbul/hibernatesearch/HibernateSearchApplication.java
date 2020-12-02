5f22f5cd9a0a6b09f79aab7c341b1789aaba112b
Hüseyin Akdoğan <hakdogan@kodcu.com>
2020-12-02 21:07:56 +0300

    hang on startup bug fixed

--- src/main/java/org/jugistanbul/hibernatesearch/HibernateSearchApplication.java
+++ src/main/java/org/jugistanbul/hibernatesearch/HibernateSearchApplication.java
@@ -2,8 +2,11 @@ package org.jugistanbul.hibernatesearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 
 @SpringBootApplication
+@EnableJpaRepositories(bootstrapMode = BootstrapMode.DEFERRED)
 public class HibernateSearchApplication
 {
 	public static void main(String[] args) {
