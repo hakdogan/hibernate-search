f0d9d966421e79627d34ff16aeeab4cfdd1208a4
Hüseyin Akdoğan <hakdogan@kodcu.com>
2020-12-02 12:04:43 +0300

    constructor injection

--- src/main/java/org/jugistanbul/hibernatesearch/StartupListener.java
+++ src/main/java/org/jugistanbul/hibernatesearch/StartupListener.java
@@ -5,6 +5,7 @@ import org.hibernate.search.mapper.orm.massindexing.MassIndexer;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.annotation.Order;
@@ -22,16 +23,18 @@ import javax.transaction.Transactional;
 @Order(0)
 class StartupListener implements ApplicationListener<ApplicationReadyEvent>
 {
+    private SearchSession searchSession;
     private final Logger logger = LoggerFactory.getLogger(StartupListener.class);
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    public StartupListener(SearchSession searchSession) {
+        this.searchSession = searchSession;
+    }
 
     @Transactional
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
 
-        SearchSession searchSession = Search.session(entityManager);
         MassIndexer indexer = searchSession.massIndexer();
         try {
             indexer.startAndWait();
