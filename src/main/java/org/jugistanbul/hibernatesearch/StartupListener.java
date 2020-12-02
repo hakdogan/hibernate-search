639da7b5625fb55a732021bfb1d41a09e1b97d2f
Hüseyin Akdoğan <hakdogan@kodcu.com>
2020-12-02 22:49:10 +0300

    refactored

--- src/main/java/org/jugistanbul/hibernatesearch/StartupListener.java
+++ src/main/java/org/jugistanbul/hibernatesearch/StartupListener.java
@@ -1,6 +1,5 @@
 package org.jugistanbul.hibernatesearch;
 
-import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.massindexing.MassIndexer;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.slf4j.Logger;
@@ -10,9 +9,6 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 /**
@@ -40,6 +36,7 @@ class StartupListener implements ApplicationListener<ApplicationReadyEvent>
             indexer.startAndWait();
         } catch (InterruptedException e) {
             logger.error("An InterruptedException was thrown", e);
+            Thread.currentThread().interrupt();
         }
     }
 
